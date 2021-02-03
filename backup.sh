cbf5cc1b798d3457158670c4ea60eb40fed9712a
Shaun Prince <shaun@techfusion.ca>
2021-02-03 01:57:42 -0800

    getting rcon working again

--- backup.sh
+++ backup.sh
@@ -6,14 +6,14 @@ export DEST_S3="s3://solidrust.net-backups/${MYNAME}"
 export INSTALL_DIR="/home/modded"
 
 # config
-/home/modded/rcon -c /home/modded/solidrust.net/rcon.yaml "server.save"
-/home/modded/rcon -c /home/modded/solidrust.net/rcon.yaml "server.writecfg"
-aws s3 sync --quiet --delete /home/modded/server/rust/cfg ${DEST_S3}/server/rust/cfg
+${INSTALL_DIR}/rcon -c ${INSTALL_DIR}/solidrust.net/rcon.yaml "server.save"
+${INSTALL_DIR}/rcon -c ${INSTALL_DIR}/solidrust.net/rcon.yaml "server.writecfg"
+aws s3 sync --quiet --delete ${INSTALL_DIR}/server/rust/cfg ${DEST_S3}/server/rust/cfg
 # data
-/home/modded/rcon -c /home/modded/solidrust.net/rcon.yaml "server.backup"
-aws s3 sync --quiet --delete /home/modded/backup ${DEST_S3}/backup
+${INSTALL_DIR}/rcon -c ${INSTALL_DIR}/solidrust.net/rcon.yaml "server.backup"
+aws s3 sync --quiet --delete ${INSTALL_DIR}/backup ${DEST_S3}/backup
 # plugins
-aws s3 sync --quiet --delete /home/modded/oxide ${DEST_S3}/oxide
+aws s3 sync --quiet --delete ${INSTALL_DIR}/oxide ${DEST_S3}/oxide
 
 #(M) Economics.json
 #(M) ServerRewards/*
