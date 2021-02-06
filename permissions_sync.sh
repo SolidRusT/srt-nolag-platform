bdbee5c30f79fadab1029b3f67bb108381769664
SmokeQc <smoke@solidrust.net>
2021-02-05 18:28:57 -0800

    fuck you

--- permissions_sync.sh
+++ permissions_sync.sh
@@ -3,15 +3,15 @@
 # example crontab
 #echo "* *    * * *   modded  /home/modded/solidrust.net/permissions_sync.sh" | sudo tee -a /etc/crontab
 # Say my name
-export MYNAME=$(hostname) 
+export MYNAME=$(hostname)
 # root of where the game server is installed
 export GAME_ROOT="/home/modded"
 # Amazon s3 destination for backups
 export S3_BUCKET="s3://solidrust.net-backups/${MYNAME}"
 # Github source for configs
 export GITHUB_ROOT="${GAME_ROOT}/solidrust.net/servers/${MYNAME}"
-# local RCON CLI config 
-export RCON_CFG="${GAME_ROOT}/solidrust.net/servers/rcon.yaml" 
+# local RCON CLI config
+export RCON_CFG="${GAME_ROOT}/solidrust.net/servers/rcon.yaml"
 
 # Update global group permissions
 ## TODO: make this a separate cron
