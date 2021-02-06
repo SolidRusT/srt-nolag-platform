a261c820df79a34e97251be8559c94c63f842ebb
Shaun Prince <shaun@techfusion.ca>
2021-02-05 17:53:27 -0800

    adding default gamedata

--- permissions_sync.sh
+++ permissions_sync.sh
@@ -2,14 +2,8 @@
 ## Configuration
 # example crontab
 #echo "* *    * * *   modded  /home/modded/solidrust.net/permissions_sync.sh" | sudo tee -a /etc/crontab
-# Say my name
-export MYNAME=$(hostname) 
 # root of where the game server is installed
 export GAME_ROOT="/home/modded"
-# Amazon s3 destination for backups
-export S3_BUCKET="s3://solidrust.net-backups/${MYNAME}"
-# Github source for configs
-export GITHUB_ROOT="${GAME_ROOT}/solidrust.net/servers/${MYNAME}"
 # local RCON CLI config 
 export RCON_CFG="${GAME_ROOT}/solidrust.net/servers/rcon.yaml" 
 
@@ -22,3 +16,6 @@ ${GAME_ROOT}/rcon -c ${RCON_CFG} "o.reload PermissionGroupSync"
 # TODO: Figure out global economics
 #(M) Economics.json
 #(M) ServerRewards/*
+#Backpack
+#Bank
+
