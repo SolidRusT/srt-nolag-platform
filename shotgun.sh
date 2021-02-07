9e67e8dfce9b5ca69749c204bf78a1dd1ca6ffe1
Shaun Prince <shaun@techfusion.ca>
2021-02-07 02:18:46 -0800

    kill the things steam

--- shotgun.sh
+++ shotgun.sh
@@ -10,9 +10,8 @@ rm -rf ${GAME_ROOT}/.mono
 # nuke files
 rm ${GAME_ROOT}/LinuxPlayer_s.debug
 rm ${GAME_ROOT}/server/solidrust/*.map
-rm ${GAME_ROOT}/server/solidrust/*.sav
+rm ${GAME_ROOT}/server/solidrust/*.sav*
 rm ${GAME_ROOT}/Compiler.x86_x64
 rm ${GAME_ROOT}/runds.sh
 rm ${GAME_ROOT}/libsteam_api.so
 rm ${GAME_ROOT}/Unity*
-
