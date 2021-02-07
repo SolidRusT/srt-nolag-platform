568191bfdf20bf4a54e17285d1ba8deddf7252c8
Shaun Prince <shaun@techfusion.ca>
2021-02-06 23:30:47 -0800

    bigger my peepee

--- solidrust-custom.sh
+++ solidrust-custom.sh
@@ -4,23 +4,31 @@ cd ${GAME_DIR}
 LOG_DATE=$(date +"%Y_%m_%d_%I_%M_%p")
 
 # Refresh Steam installation
-echo "===> Updating Steam files...\n"
+echo "===> Updating Steam files..."
 steamcmd +login anonymous +force_install_dir ~/ +app_update 258550 +quit
-echo "===> Validating installed Steam components...\n"
+echo "===> Validating installed Steam components..."
 steamcmd +login anonymous +force_install_dir ~/ +app_update 258550 validate +quit
 
 # Update uMod platform
-echo "===> Updating uMod...\n"
-wget https://umod.org/games/rust/download/develop -O Oxide.Rust.zip
-unzip -o Oxide.Rust.zip
-rm Oxide.Rust.zip
+echo "===> Updating uMod..."
+wget https://umod.org/games/rust/download/develop -O \
+    Oxide.Rust.zip && \
+    unzip -o Oxide.Rust.zip && \
+    rm Oxide.Rust.zip
 
 # Integrate discord binary
-echo "===> Downloading discord binary...\n"
-wget https://umod.org/extensions/discord/download -O ~/RustDedicated_Data/Managed/Oxide.Ext.Discord.dll
+echo "===> Downloading discord binary..."
+wget https://umod.org/extensions/discord/download -O \
+    ${GAME_DIR}/RustDedicated_Data/Managed/Oxide.Ext.Discord.dll
+
+# Integrate RustEdit binary
+echo "===> Downloading RustEdit.io binary..."
+wget https://github.com/k1lly0u/Oxide.Ext.RustEdit/raw/master/Oxide.Ext.RustEdit.dll -O \
+    ${GAME_DIR}/RustDedicated_Data/Managed/Oxide.Ext.RustEdit.dll
+
 
 # Launch game server
-echo "===> Touching my peepee...\n"
+echo "===> Touching my peepee..."
 exec ./RustDedicated -batchmode -nographics -silent-crashes \
     -server.ip 0.0.0.0 \
     -rcon.ip 0.0.0.0 \
@@ -40,6 +48,6 @@ exec ./RustDedicated -batchmode -nographics -silent-crashes \
     -server.savebackupcount "2" \
     -logfile 2>&1 "RustDedicated-${LOG_DATE}.log"
 
-echo "I'm done! (finished)\n"
+echo "I'm done! (finished)"
 
 exit 0
