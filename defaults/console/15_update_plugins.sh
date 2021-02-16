329f14b131b22bc9b00a0b385e7a5c6418c7ae30
Shaun Prince <shaun@techfusion.ca>
2021-02-16 00:12:02 -0800

    reset plugin update script

--- defaults/console/15_update_plugins.sh
+++ defaults/console/15_update_plugins.sh
@@ -4,7 +4,7 @@
 #
 ## crontab example:
 #      M H    D ? Y
-#echo "0 5    * * *   ${USER}  ${HOME}/solidrust.net/defaults/console/60_update_plugins.sh" | sudo tee -a /etc/crontab
+#echo "15 5    * * *   ${USER}  ${HOME}/solidrust.net/defaults/console/15_update_plugins.sh" | sudo tee -a /etc/crontab
 
 # Pull global env vars
 source ${HOME}/solidrust.net/defaults/env_vars.sh
