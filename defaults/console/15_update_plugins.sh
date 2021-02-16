08f5318c9a5f8774fe7ec163d1f600854b4d4994
Shaun Prince <shaun@techfusion.ca>
2021-02-16 00:23:16 -0800

    update plugins

--- defaults/console/15_update_plugins.sh
+++ defaults/console/15_update_plugins.sh
@@ -10,6 +10,7 @@
 source ${HOME}/solidrust.net/defaults/env_vars.sh
 
 # lock the repo from being updated while we complete this job
+echo "=====> locking repo <=====" | tee -a ${LOGS}
 touch ${SOLID_LCK}
 
 # Delete and refresh SolidRusT repo
@@ -17,18 +18,23 @@ rm -rf ${GITHUB_ROOT}
 cd ${HOME} && \
 git clone git@github.com:suparious/solidrust.net.git | tee -a ${LOGS}
 
+export PLUGIN_COUNT=$(ls -1 "${SERVER_GLOBAL}/oxide/plugins | wc -l")
+echo "=====> Updating ${PLUGIN_COUNT} plugins! <=====" | tee -a ${LOGS}
+
 plugins=$(ls -1 "${SERVER_GLOBAL}/oxide/plugins")
 
 for plugin in ${plugins[@]}; do
-    echo "updating $plugin" | tee -a ${LOGS}
+    echo "==> updating $plugin <==" | tee -a ${LOGS}
     echo wget -N "https://umod.org/plugins/$plugin" -O  "${SERVER_GLOBAL}/oxide/plugins/$plugin" | tee -a ${LOGS}
     sleep 3
 done
 
 # commit any updates
+cd ${GITHUB_ROOT}
 git add . && \
 git commit -m "auto plugin update" && \
 git push  | tee -a ${LOGS}
 
 # unlock the repo
+echo "=====> unlocking repo <=====" | tee -a ${LOGS}
 rm ${SOLID_LCK}
