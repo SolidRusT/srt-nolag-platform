7affe302d8112fe44d46436d42801cd129af3c07
Shaun Prince <shaun@techfusion.ca>
2021-02-21 12:47:07 -0800

    enable full backups

--- defaults/9-39_backup_oxide.sh
+++ defaults/9-39_backup_oxide.sh
@@ -4,12 +4,30 @@
 #
 ## crontab example:
 #      M H    D ? Y
-#echo "9,39 *    * * *   ${USER}  /bin/sh -c ${HOME}/solidrust.net/defaults/9_backup_oxide.sh" | sudo tee -a /etc/crontab
+#echo "9,39 *    * * *   ${USER}  /bin/sh -c ${HOME}/solidrust.net/defaults/9-39_backup_oxide.sh" | sudo tee -a /etc/crontab
 
 # Pull global env vars
 source ${HOME}/solidrust.net/defaults/env_vars.sh
 me=$(basename -- "$0")
 echo "====> Starting ${me}: ${LOG_DATE}" | tee -a ${LOGS}
+
+# Check for RCON
+if [ -f "${GAME_ROOT}/rcon" ]; then
+    echo "rcon binary found" # no need to log this
+else
+    echo "No rcon found here, downloading it..." | tee -a ${LOGS}
+    wget https://github.com/gorcon/rcon-cli/releases/download/v0.9.0/rcon-0.9.0-amd64_linux.tar.gz
+    tar xzvf rcon-0.9.0-amd64_linux.tar.gz
+    mv rcon-0.9.0-amd64_linux/rcon ${GAME_ROOT}/rcon
+    rm -rf rcon-0.9.0-amd64_linux*
+fi
+
+${GAME_ROOT}/rcon --log ${LOGS} --config ${RCON_CFG} "server.writecfg"
+sleep 1
+${GAME_ROOT}/rcon --log ${LOGS} --config ${RCON_CFG} "server.save"
+sleep 5
+${GAME_ROOT}/rcon --log ${LOGS} --config ${RCON_CFG} "server.backup"
+sleep 10
 
 CONTENTS=(
     oxide
