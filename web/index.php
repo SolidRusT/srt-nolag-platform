84e32c5219edc8cb5194ed35537d77330b7d4c19
Shaun Prince <shaun@techfusion.ca>
2021-07-13 22:26:28 -0700

    SRT v1.5.9 hotfix

--- web/index.php
+++ web/index.php
@@ -32,7 +32,7 @@ body {
 	
 	<script type="application/javascript">window.addEventListener('message',function(e){if(e.data.uid&&e.data.type==='sizeUpdate'){var i = document.querySelector('iframe[name="'+e.data.uid+'"]');i.style.width = e.data.payload.width;i.style.height = e.data.payload.height;}});</script><iframe src="https://cdn.battlemetrics.com/b/horizontal500x80px/11593491.html?_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Imhvcml6b250YWw1MDB4ODBweCIsInNlcnZlciI6MTE1OTM0OTEsIm9wdGlvbnMiOnsiZm9yZWdyb3VuZCI6IiNFRUVFRUUiLCJiYWNrZ3JvdW5kIjoiIzIyMjIyMiIsImxpbmVzIjoiIzMzMzMzMyIsImxpbmtDb2xvciI6IiNmZmUzMDAiLCJjaGFydENvbG9yIjoiI0ZGMDcwMCJ9LCJsaXZlVXBkYXRlcyI6dHJ1ZSwidXNlcl9pZCI6MzU4MjIwLCJpYXQiOjE2MjM2MDU4OTh9.BJgFohCkxMaXHFEQzEiA4OwytUIPvKCH5nGwivQweR4" frameborder=0 style="border:0" name="zweam"></iframe><br>
 	
-	<script type="application/javascript">window.addEventListener('message',function(e){if(e.data.uid&&e.data.type==='sizeUpdate'){var i = document.querySelector('iframe[name="'+e.data.uid+'"]');i.style.width = e.data.payload.width;i.style.height = e.data.payload.height;}});</script><iframe src="https://cdn.battlemetrics.com/b/horizontal500x80px/11594431.html?_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Imhvcml6b250YWw1MDB4ODBweCIsInNlcnZlciI6MTE1OTQ0MzEsIm9wdGlvbnMiOnsiZm9yZWdyb3VuZCI6IiNFRUVFRUUiLCJiYWNrZ3JvdW5kIjoiIzIyMjIyMiIsImxpbmVzIjoiIzMzMzMzMyIsImxpbmtDb2xvciI6IiNmZmUzMDAiLCJjaGFydENvbG9yIjoiI0ZGMDcwMCJ9LCJsaXZlVXBkYXRlcyI6dHJ1ZSwidXNlcl9pZCI6MzU4MjIwLCJpYXQiOjE2MjM2MDU4NTh9.bVYKTdUOSgdUV8dZ8OPIO43F9akNrkGXX0fUhic6m48" frameborder=0 style="border:0" name="ioqcb"></iframe>
+	
 	  
 	<object data='https://www.youtube.com/embed/rVZktA3WwE4?autoplay=1' 
         width='560px' height='315px'>
@@ -43,6 +43,7 @@ body {
 	 
   </tr>
 </table>
+<a href="https://topg.org/rust-servers/server-631719" target="_blank"><img src="https://topg.org/topg2.gif" width="88" height="31" border="0" alt="Solidrust 5x trio - Rust server"></a>
 <a href="https://ipv6-test.com/validate.php?url=referer"><img src="https://ipv6-test.com/button-ipv6-80x15.png"
       alt="ipv6 ready" title="ipv6 ready" border="0" /></a>
 </body>
