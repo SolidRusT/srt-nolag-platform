f9bed83b055212a7f0b161c4bfa0845ec060a178
Shaun Prince <shaun@techfusion.ca>
2021-08-23 10:17:36 -0700

    two migration

--- servers/two/env_vars.sh
+++ servers/two/env_vars.sh
@@ -8,9 +8,9 @@ export SERVER_LOGS="${GAME_ROOT}/RustDedicated.log"
 # toggle custom maps
 export CUSTOM_MAP="disabled" # enabled / disabled
 export CUSTOM_MAP_URL="" #  only if CUSTOM_MAP is "enabled"
-export WORLD_SIZE="3500"
+export WORLD_SIZE="3750"
 # Current Map seed
 export SEED=$(cat ${GAME_ROOT}/server.seed)
 # Discord Settings
 export WEBHOOK=https://discordapp.com/api/webhooks/869389187667877898/7ufiQxBLWqs2FTXJT2y3AcGAqstL129Lg6WPbuRBB3WRrPHJwbIdxYyHqosSgT-NNqtp
-export CORDNAME="Nine Watchdog"
+export CORDNAME="Two Watchdog"
