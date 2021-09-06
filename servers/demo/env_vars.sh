3c73472757c09e758ad1ec90540560c21d5c52e1
Shaun Prince <shaun@techfusion.ca>
2021-09-05 18:27:22 -0700

    Launching demo build for first time

--- servers/demo/env_vars.sh
+++ servers/demo/env_vars.sh
@@ -8,7 +8,7 @@ export SERVER_LOGS="${GAME_ROOT}/RustDedicated.log"
 # toggle custom maps
 export CUSTOM_MAP="disabled" # enabled / disabled
 export CUSTOM_MAP_URL="" #  only if CUSTOM_MAP is "enabled"
-export WORLD_SIZE="2000"
+export WORLD_SIZE="2700"
 # Current Map seed
 export SEED=$(cat ${GAME_ROOT}/server.seed)
 # Discord Settings
