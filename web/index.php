6bd1f29090d541c2e444c50e8d41b8db337b857a
Shaun Prince <shaun@techfusion.ca>
2022-01-11 16:53:32 -0800

    update website banners

--- web/index.php
+++ web/index.php
@@ -15,17 +15,17 @@ if (!isset($_SESSION['steamid'])) {
 
 <div class="container-fluid">
 <div class="row">
-<div class="col-sm-5">
+<div class="col-sm-3">
   <div class="card">
 	<div class="card-body">
     <img src="/images/SoldRust_Header_nine.png" class="card-img-top" alt="connect Nine.SolidRusT.net:28015">
-	  <h4 class="card-title">SRT 5x NoBP - Solo/Duo/Trio</h4>
+	  <h4 class="card-title">SolidRusT 10x NoLAG</h4>
 	  <a href="steam://connect/nine.solidrust.net:28015" class="btn btn-primary">connect nine.solidrust.net:28015</a>
-	  <p class="card-text"><h6>This server is a fast paced 5x with custom mods.</h6>
+	  <p class="card-text"><h6>This server is a fast paced 10x with custom mods.</h6>
 		<ul>
-		  <li>Map Wipe every MONDAY 10AM-PST</li>
+		  <li>Map Wipe every MONDAY 11AM-PST</li>
 		  <li>Full wipe on the first THURSDAY of the month</li>
-		  <li>Max group size of 10 players, including alliances</li>
+		  <li>No max group size</li>
 		  <li>70min Days, 10min Brighter Nights</li>
 		  <li>Kits, Skins, Spawn Mini/Sedan/Recycler</li>
 		  <li>Craft your own recycler, </li>
@@ -39,11 +39,72 @@ if (!isset($_SESSION['steamid'])) {
 		  <li>Realistic weather, health effects, bullet impact and explosions</li>
 		</ul>
 	  </p>
-	  <script type="application/javascript">window.addEventListener('message',function(e){if(e.data.uid&&e.data.type==='sizeUpdate'){var i = document.querySelector('iframe[name="'+e.data.uid+'"]');i.style.width = e.data.payload.width;i.style.height = e.data.payload.height;}});</script><iframe src="https://cdn.battlemetrics.com/b/standardVertical/11593491.html?_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InN0YW5kYXJkVmVydGljYWwiLCJzZXJ2ZXIiOjExNTkzNDkxLCJvcHRpb25zIjp7ImZvcmVncm91bmQiOiIjODUwMDAwIiwibGlua0NvbG9yIjoiIzAwMDAwMCIsImxpbmVzIjoiIzg1MDAwMCIsImJhY2tncm91bmQiOiIjZmZmZmZmIiwiY2hhcnQiOiJ0aW1lOjFNIiwiY2hhcnRDb2xvciI6IiMwMDEzYWMiLCJtYXhQbGF5ZXJzSGVpZ2h0IjoiMzAwIn0sImxpdmVVcGRhdGVzIjp0cnVlLCJ1c2VyX2lkIjozNTgyMjAsImlhdCI6MTYyODY1Nzc2Nn0.yHhEQSkhA8kijr2RAtkj_vaifFV4_cUCcDVjkyCEDl8" frameborder=0 style="border:0" name="ilqin"></iframe><br>
-		<script src="https://rust-servers.net/embed.js?id=162953&type=votes"></script>
+	  <script type="application/javascript">window.addEventListener('message',function(e){if(e.data.uid&&e.data.type==='sizeUpdate'){var i = document.querySelector('iframe[name="'+e.data.uid+'"]');i.style.width = e.data.payload.width;i.style.height = e.data.payload.height;}});</script><iframe src="https://cdn.battlemetrics.com/b/standardVertical/11593491.html?_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InN0YW5kYXJkVmVydGljYWwiLCJzZXJ2ZXIiOjExNTkzNDkxLCJvcHRpb25zIjp7ImZvcmVncm91bmQiOiIjNTYwMDAwIiwibGlua0NvbG9yIjoiIzAwMDAwMCIsImxpbmVzIjoiIzMzMzMzMyIsImJhY2tncm91bmQiOiIjZmZmZmZmIiwiY2hhcnQiOiJ0aW1lOjFNIiwiY2hhcnRDb2xvciI6IiNGRjA3MDAiLCJtYXhQbGF5ZXJzSGVpZ2h0IjoiMzAwIn0sImxpdmVVcGRhdGVzIjp0cnVlLCJ1c2VyX2lkIjozNTgyMjAsImlhdCI6MTY0MTk0ODI3MX0.8y6vW7KL3z4n4z5wq1TrcbD71jQYFRX5fpHTwxCw-xQ" frameborder=0 style="border:0" name="ilmwk"></iframe><br>
+	  <script src="https://rust-servers.net/embed.js?id=165161&type=votes"></script>
       <p class="card-text"><small class="text-muted">Last updated less than 1 min ago</small></p>
 	</div>
   </div>
+</div>
+<div class="col-sm-3">
+  <div class="card">
+	<div class="card-body">
+    <img src="/images/SoldRust_Header_demo.png" class="card-img-top" alt="connect blaze.hq.SolidRusT.net:28015">
+	  <h4 class="card-title">SolidRusT 100x NoLAG</h4>
+	  <a href="steam://connect/blaze.hq.solidrust.net:28015" class="btn btn-primary">connect blaze.hq.solidrust.net:28015</a>
+	  <p class="card-text"><h6>This server is a fast paced 100x with custom mods.</h6>
+		<ul>
+		  <li>Map Wipe every WEDNESDAY 11AM-PST</li>
+		  <li>Full wipe on the first THURSDAY of the month</li>
+		  <li>No max group size</li>
+		  <li>70min Days, 10min Brighter Nights</li>
+		  <li>Kits, Skins, Spawn Mini/Sedan/Recycler</li>
+		  <li>Craft your own recycler, </li>
+		  <li>Raidable Bases / Roaming NPC Raiders</li>
+		  <li>5x Gather, Improved stack sizes</li>
+		  <li>No Blueprints, larger workbench area</li>
+		  <li>Supply Drops, airdrops and custom NPC events</li>
+		  <li>Custom Loot tables, Recycle any item for useful mats</li>
+		  <li>Additional monument puzzles and utilities</li>
+		  <li>5 additional custom radio stations (live DJs)</li>
+		  <li>Realistic weather, health effects, bullet impact and explosions</li>
+		</ul>
+	  </p>
+	  <script type="application/javascript">window.addEventListener('message',function(e){if(e.data.uid&&e.data.type==='sizeUpdate'){var i = document.querySelector('iframe[name="'+e.data.uid+'"]');i.style.width = e.data.payload.width;i.style.height = e.data.payload.height;}});</script><iframe src="https://cdn.battlemetrics.com/b/standardVertical/13975509.html?_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InN0YW5kYXJkVmVydGljYWwiLCJzZXJ2ZXIiOjEzOTc1NTA5LCJvcHRpb25zIjp7ImZvcmVncm91bmQiOiIjNTYwMDAwIiwibGlua0NvbG9yIjoiIzAwMDAwMCIsImxpbmVzIjoiIzMzMzMzMyIsImJhY2tncm91bmQiOiIjZmZmZmZmIiwiY2hhcnQiOiJ0aW1lOjFNIiwiY2hhcnRDb2xvciI6IiNGRjA3MDAiLCJtYXhQbGF5ZXJzSGVpZ2h0IjoiMzAwIn0sImxpdmVVcGRhdGVzIjp0cnVlLCJ1c2VyX2lkIjozNTgyMjAsImlhdCI6MTY0MTk0ODQyOX0.OUD3J9df35tJqWh1Ks2dyMK3g79zfkfrMXrHOXm8mUc" frameborder=0 style="border:0" name="udumw"></iframe><br>
+	  <script src="https://rust-servers.net/embed.js?id=165515&type=votes"></script>
+      <p class="card-text"><small class="text-muted">Last updated less than 1 min ago</small></p>
+	</div>
+  </div>
+</div>
+<div class="col-sm-3">
+  <div class="card">
+	<div class="card-body">
+    <img src="/images/SoldRust_Header_demo.png" class="card-img-top" alt="connect demo.SolidRusT.net:28015">
+	  <h4 class="card-title">SolidRusT 1000x NoLAG</h4>
+	  <a href="steam://connect/demo.solidrust.net:28015" class="btn btn-primary">connect demo.solidrust.net:28015</a>
+	  <p class="card-text"><h6>This server is a fast paced 1000x with custom mods.</h6>
+		<ul>
+		  <li>Map Wipe every WEDNESDAY 11AM-PST</li>
+		  <li>Full wipe on the first THURSDAY of the month</li>
+		  <li>No max group size</li>
+		  <li>70min Days, 10min Brighter Nights</li>
+		  <li>Kits, Skins, Spawn Mini/Sedan/Recycler</li>
+		  <li>Craft your own recycler, </li>
+		  <li>Raidable Bases / Roaming NPC Raiders</li>
+		  <li>5x Gather, Improved stack sizes</li>
+		  <li>No Blueprints, larger workbench area</li>
+		  <li>Supply Drops, airdrops and custom NPC events</li>
+		  <li>Custom Loot tables, Recycle any item for useful mats</li>
+		  <li>Additional monument puzzles and utilities</li>
+		  <li>5 additional custom radio stations (live DJs)</li>
+		  <li>Realistic weather, health effects, bullet impact and explosions</li>
+		</ul>
+	  </p>
+	  <script type="application/javascript">window.addEventListener('message',function(e){if(e.data.uid&&e.data.type==='sizeUpdate'){var i = document.querySelector('iframe[name="'+e.data.uid+'"]');i.style.width = e.data.payload.width;i.style.height = e.data.payload.height;}});</script><iframe src="https://cdn.battlemetrics.com/b/standardVertical/12670631.html?_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InN0YW5kYXJkVmVydGljYWwiLCJzZXJ2ZXIiOjEyNjcwNjMxLCJvcHRpb25zIjp7ImZvcmVncm91bmQiOiIjNTYwMDAwIiwibGlua0NvbG9yIjoiIzAwMDAwMCIsImxpbmVzIjoiIzMzMzMzMyIsImJhY2tncm91bmQiOiIjZmZmZmZmIiwiY2hhcnQiOiJ0aW1lOjFNIiwiY2hhcnRDb2xvciI6IiNGRjA3MDAiLCJtYXhQbGF5ZXJzSGVpZ2h0IjoiMzAwIn0sImxpdmVVcGRhdGVzIjp0cnVlLCJ1c2VyX2lkIjozNTgyMjAsImlhdCI6MTY0MTk0ODQ4OX0.MJ4KkVARUC0chpl0V76MwSLhM5q8jvvGYCMsx2iMDWE" frameborder=0 style="border:0" name="bffwh"></iframe><br>
+	  <script src="https://rust-servers.net/embed.js?id=165160&type=votes"></script>
+      <p class="card-text"><small class="text-muted">Last updated less than 1 min ago</small></p>
+	</div>
+  </div>
+</div>
 </div>
 </div>
 </div>
