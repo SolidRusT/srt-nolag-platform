3c8abc06c0874a26da77c92b2359f845c4c70024
Shaun Prince <shaun@techfusion.ca>
2022-01-28 11:23:26 -0800

    update profile page

--- web/profile.php
+++ web/profile.php
@@ -14,8 +14,15 @@ if (!isset($_SESSION['steamid'])) {
         if (isset($_SESSION['user_id'])) {
             $discordid = $_SESSION['user_id'];
             $steamid = $_SESSION['steamid'];
-            echo "DiscordID: $discordid<br>";
-            echo "SteamID: $steamid</p>";
+            echo "<div class=\"card\" style=\"width: 18rem;\">";
+            // <img src="..." class="card-img-top" alt="...">
+            echo "<div class=\"card-body\">";
+            echo "<h5 class=\"card-title\">$profile_name&lsquo;s profile.</h5>";
+            echo "<p class=\"card-text\">DiscordID: $discordid<br>";
+            echo "<p class=\"card-text\">SteamID: $steamid</p>";
+            // echo "<a href=\"#\" class=\"btn btn-primary\">Go somewhere</a>";
+            echo "</div>";
+            echo "</div>";
         } else {
             echo "</p><p class=\"bg-danger\">Can't find Discord user</p>";
         }
