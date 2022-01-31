d2ecc28cfb2ed09e7e099912589ab052656c1381
Shaun Prince <shaun@techfusion.ca>
2022-01-31 14:14:38 -0800

    renaming data connections to be unique

--- web/scoreboard.php
+++ web/scoreboard.php
@@ -1,16 +1,16 @@
 <?php include 'nav_bar.php';?>
 <?php
 // Create connection to XPerience
-$db = "XPerience";
-$details = "mysql:dbname=$db;host=$db_host";
-$database = new PDO($details, $user, $pass);
+$db_name = "XPerience";
+$db_conn = "mysql:host=$db_host;dbname=$db_name";
+$xpstatsdb = new PDO($db_conn, $user, $pass);
 // Check connection
-if ($database->connect_error) {
+if ($xpstatsdb->connect_error) {
   die("Connection failed: " . $database->connect_error);
 }
 // Get player list
 $sql = "SELECT displayname, level, experience, status  FROM XPerience";
-$result = $database->query($sql);
+$result = $xpstatsdb->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
@@ -20,6 +20,6 @@ if ($result->num_rows > 0) {
 } else {
   echo "0 results";
 }
-$database->close();
+$xpstatsdb->close();
 ?>
 <?php include 'footer.php';?>
