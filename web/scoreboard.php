4b6e4be8e2a10cc6e267c100a3df7f2bd534f351
Shaun Prince <shaun@techfusion.ca>
2022-01-31 14:46:25 -0800

    update scoreboard db conn

--- web/scoreboard.php
+++ web/scoreboard.php
@@ -4,24 +4,21 @@
 $dsn = "mysql:host=$db_host;dbname=XPerience";
 $xpstatsdb = new PDO($dsn, $user, $pass);
 $xpstatsdb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// Check connection
-if(!empty($displayname)){
-  $query = "SELECT displayname, level, experience, status  FROM XPerience;";
-  $data = [$displayname,$level,$experience,$status];
-  $dbc->prepare($query)->execute($data);
-  echo "Thank you. The record has been sent successfully.<br><br>";}
-else{
-  echo '<h1>Please use the contact form or don\'t leave an empty field!</h1>';
-}
+
+$query = "SELECT displayname, level, experience, status  FROM XPerience;";
+$data = [$displayname,$level,$experience,$status];
+//$xpstats->prepare($query)->execute($data);
+$xpstats = $xpstatsdb->query($query);
 // Get player list
-if ($result->num_rows > 0) {
+if ($xpstats->num_rows > 0) {
   // output data of each row
-  while($row = $result->fetch_assoc()) {
+  while($row = $xpstats->fetch_assoc()) {
     echo "Player: " . $row["displayname"]. " - Level: " . $row["level"]. " " . $row["experience"]. " - Status: " . $row["status"]. "<br>";
   }
 } else {
   echo "0 results";
 }
+// Close the DB
 $xpstatsdb->close();
 ?>
 <?php include 'footer.php';?>
