a9e65373f09a9f7517e93a0f2c212f3174f4b167
Shaun Prince <shaun@techfusion.ca>
2022-01-31 17:02:23 -0800

    added multi-db test

--- web/scoreboard.php
+++ web/scoreboard.php
@@ -1,35 +1,94 @@
 <?php include 'nav_bar.php';?>
 <?php
-// Create connection to XPerience
-$dsn = "mysql:host=$db_host;dbname=XPerience";
-$xpstatsdb = new PDO($dsn, $user, $pass);
-$xpstatsdb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// Get player data
-$query = $xpstatsdb->query("SELECT * FROM XPerience ORDER BY level DESC limit 0,100");
-// print a nice table
-echo "<table style=\"background-color:#FFFFE0;\" border = '2'>
-  <tr style=\"background-color:#BDB76B;color:#ffffff;\">
-  <th>Level &nbsp;&nbsp;</th>
-  <th>Players(Top 100)</th>
-  <th>XP</th>
-  <th>Stats</th>
-  <th>Skills</th>
-  <th>Status</th>
-  </tr>";
-while ($row = $query->fetch()) {
-  echo "<tr>";
+$nine_dsn = "mysql:host=$db_host;dbname=xpstats_nine";
+$eleven_dsn = "mysql:host=$db_host;dbname=xpstats_eleven";
+$demo_dsn = "mysql:host=$db_host;dbname=xpstats_demo";
+$xpstatsdb_nine = new PDO($nine_dsn, $user, $pass);
+$xpstatsdb_nine->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$xpstatsdb_eleven = new PDO($eleven_dsn, $user, $pass);
+$xpstatsdb_eleven->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$xpstatsdb_demo = new PDO($demo_dsn, $user, $pass);
+$xpstatsdb_demo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
+<table class="table table-striped">
+<thead>
+<tr>
+  <th>US West 10x</th>
+  <th>CA West 100x</th>
+  <th>US East 1000x</th>
+</tr>
+</thead>
+<tfoot>
+<tr>
+  <th>US West 10x</th>
+  <th>CA West 100x</th>
+  <th>US East 1000x</th>
+</tr>
+</tfoot>
+<tbody>
+<tr>
+  <td>Level &nbsp;&nbsp;</td>
+  <td>Players(Top 100)</td>
+  <td>XP</td>
+  <td>Stats</td>
+  <td>Skills</td>
+  <td>Status</td>
+</tr>
+<tr>
+<?php
+  $query = $xpstatsdb_nine->query("SELECT * FROM XPerience ORDER BY level DESC limit 0,100");
+  while ($row = $query->fetch()) {
   echo "<td align=\"center\">" . $row['level'] . "</td>";
   echo "<td align=\"left\">" . $row['displayname'] ."</td>";
   echo "<td align=\"left\">" . $row['experience'] . "</td>";
   echo "<td align=\"right\">" . $row['statpoint'] . "</td>";
   echo "<td align=\"right\">" . $row['skillpoint'] . "</td>";
   echo "<td align=\"center\">" . $row['Status'] . "</td>";
-  echo "</tr>";
 }
-echo "</table>";
-// show sql version
-$sqlver = $xpstatsdb->query("SELECT VERSION()");
-$version = $sqlver->fetch();
-echo $version[0] . PHP_EOL;
 ?>
+</tr>
+<tr>
+<td>Level &nbsp;&nbsp;</td>
+<td>Players(Top 100)</td>
+<td>XP</td>
+<td>Stats</td>
+<td>Skills</td>
+<td>Status</td>
+</tr>
+<tr>
+<?php
+  $query = $xpstatsdb_eleven->query("SELECT * FROM XPerience ORDER BY level DESC limit 0,100");
+  while ($row = $query->fetch()) {
+  echo "<td align=\"center\">" . $row['level'] . "</td>";
+  echo "<td align=\"left\">" . $row['displayname'] ."</td>";
+  echo "<td align=\"left\">" . $row['experience'] . "</td>";
+  echo "<td align=\"right\">" . $row['statpoint'] . "</td>";
+  echo "<td align=\"right\">" . $row['skillpoint'] . "</td>";
+  echo "<td align=\"center\">" . $row['Status'] . "</td>";
+}
+?>
+</tr>
+<tr>
+<td>Level &nbsp;&nbsp;</td>
+<td>Players(Top 100)</td>
+<td>XP</td>
+<td>Stats</td>
+<td>Skills</td>
+<td>Status</td>
+</tr>
+<tr>
+<?php
+  $query = $xpstatsdb_demo->query("SELECT * FROM XPerience ORDER BY level DESC limit 0,100");
+  while ($row = $query->fetch()) {
+  echo "<td align=\"center\">" . $row['level'] . "</td>";
+  echo "<td align=\"left\">" . $row['displayname'] ."</td>";
+  echo "<td align=\"left\">" . $row['experience'] . "</td>";
+  echo "<td align=\"right\">" . $row['statpoint'] . "</td>";
+  echo "<td align=\"right\">" . $row['skillpoint'] . "</td>";
+  echo "<td align=\"center\">" . $row['Status'] . "</td>";
+}
+?>
+</tr>
+</tbody>
+</table>
 <?php include 'footer.php';?>
