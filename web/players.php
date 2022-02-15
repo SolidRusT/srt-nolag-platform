4e36121adb5712da1a996e0b21ca617a8fd7263b
Shaun Prince <suparious@solidrust.net>
2022-02-15 07:26:49 -0800

    glob all data

--- web/players.php
+++ web/players.php
@@ -10,30 +10,29 @@ $RustPlayers_nine->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
           <thead>
             <tr>
               <th>Player</th>
-              <th>Last Seen</th>
               <th>Time Played</th>
+              <th>Last Seen</th>
               <th>First Seen</th>
             </tr>
           </thead>
           <tfoot>
             <tr>
               <th>Player</th>
-              <th>Last Seen</th>
               <th>Time Played</th>
+              <th>Last Seen</th>
               <th>First Seen</th>
             </tr>
           </tfoot>
           <tbody>
             <?php
-              $query = $RustPlayers_nine->query("SELECT 'name','steamid','Last Seen','Time Played','First Connection' FROM west ORDER BY level DESC limit 0,100");
+              $query = $RustPlayers_nine->query("SELECT * FROM west ORDER BY 'Time Played' DESC limit 0,100");
               while ($row = $query->fetch()) {
                 echo "<tr>";
                 echo "<th scope=\"row\" align=\"center\">" . $row['level'] . "</th>";
                 echo "<td>" . $row['name'] ."</td>";
-                echo "<td>" . $row['Last Seen'] . "</td>";
                 echo "<td>" . $row['Time Played'] . "</td>";
+                echo "<td>" . $row['Last Seen'] . "</td>";
                 echo "<td>" . $row['First Connection'] . "</td>";
-                echo "<td>" . $row['steamid'] . "</td>";
                 echo "</tr>";
               }
             ?>
