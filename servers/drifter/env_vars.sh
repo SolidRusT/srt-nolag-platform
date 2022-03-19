a3985da34e75372c21cbccac72213279cfd10d49
Shaun Prince <shaun@techfusion.ca>
2022-03-18 22:19:29 -0700

    prepare 100x for first start

--- servers/drifter/env_vars.sh
+++ servers/drifter/env_vars.sh
@@ -9,9 +9,9 @@ export SERVER_LOGS="${GAME_ROOT}/RustDedicated.log"
 export WORLD_SIZE="2700"
 # toggle custom maps
 export CUSTOM_MAP="enabled" # enabled / disabled
-export CUSTOM_MAP_URL="https://solidrust.net/maps/SRT-NoLAG-20211104.map" #  only if CUSTOM_MAP is "enabled"
+export CUSTOM_MAP_URL="https://solidrust.net/maps/Booty_Island_ver2.map" #  only if CUSTOM_MAP is "enabled"
 # Current Map seed
 export SEED=$(cat ${GAME_ROOT}/server.seed)
 # Discord Settings
-export WEBHOOK=https://discordapp.com/api/webhooks/869389187667877898/7ufiQxBLWqs2FTXJT2y3AcGAqstL129Lg6WPbuRBB3WRrPHJwbIdxYyHqosSgT-NNqtp
-export CORDNAME="Demo Watchdog"
+export WEBHOOK=https://discord.com/api/webhooks/954609421185593365/fyYcxae7shMAlrlet2mapqVRGXMxCW_8GzBrcZmS77GpJHu6z2uVC0Ay1hchQrtLJeGs
+export CORDNAME="ca-100x Watchdog"
