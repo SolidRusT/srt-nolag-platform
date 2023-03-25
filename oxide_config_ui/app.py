c01dc047fdcefdf95597099fa3610d32950f9b8d
Shaun Prince <suparious@solidrust.net>
2023-03-25 00:38:18 -0700

    gitignore the cache

--- oxide_config_ui/app.py
+++ oxide_config_ui/app.py
@@ -7,16 +7,20 @@ config = configparser.ConfigParser()
 config.read("config.ini")
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql://{config.get('mysql', 'user')}:{config.get('mysql', 'password')}@{config.get('mysql', 'host')}:{config.get('mysql', 'port')}/{config.get('mysql', 'database')}"
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = f"mysql://{config.get('mysql', 'user')}:{config.get('mysql', 'password')}@{config.get('mysql', 'host')}:{config.get('mysql', 'port')}/{config.get('mysql', 'database')}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+
 
 class ConfigValues(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     server_name = db.Column(db.String(50), nullable=False)
     key_name = db.Column(db.String(50), nullable=False)
     value = db.Column(db.String(255), nullable=False)
+
 
 @app.route("/api/config_values", methods=["GET", "POST"])
 def manage_config_values():
@@ -30,13 +34,20 @@ def manage_config_values():
 
     elif request.method == "POST":
         data = request.get_json()
-        new_config_value = ConfigValues(server_name=data["server_name"], key_name=data["key_name"], value=data["value"])
+        new_config_value = ConfigValues(
+            server_name=data["server_name"],
+            key_name=data["key_name"],
+            value=data["value"],
+        )
         db.session.add(new_config_value)
         db.session.commit()
         return jsonify(new_config_value.to_dict()), 201
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run()
